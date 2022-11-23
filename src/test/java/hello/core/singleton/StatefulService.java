efcaec700c32ad7990a38ff5913530ddbfab8c73
chaechae <songchaechaes@gmail.com>
2022-11-23 17:38:01 +0900

    add SingletonTest

---
+++ src/test/java/hello/core/singleton/StatefulService.java
@@ -0,0 +1,14 @@
+package hello.core.singleton;
+
+public class StatefulService {
+    private int price; // 상태 유지하는 필드 10000 -> 20000
+
+    public void order(String name, int price) {
+        System.out.println("name = " + name + " price = " + price);
+        this.price = price; // 여기가 문제
+    }
+
+    public int getPrice() {
+        return price;
+    }
+}
