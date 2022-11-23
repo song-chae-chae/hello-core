0fc0ba4a1f4dfdab1a178c3c633153e570ef8a17
chaechae <songchaechaes@gmail.com>
2022-11-23 11:01:40 +0900

    modify Services for dependency injection

--- src/test/java/hello/core/member/MemberServiceImplTest.java
+++ src/test/java/hello/core/member/MemberServiceImplTest.java
@@ -1,12 +1,18 @@
 package hello.core.member;
 
+import hello.core.AppConfig;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class MemberServiceImplTest {
-    MemberService service = new MemberServiceImpl();
+    MemberService memberService;
+
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+    }
 
     @Test
     public void join() {
@@ -14,8 +20,8 @@ class MemberServiceImplTest {
         Member member = new Member(1L, "memberA", Grade.BASIC);
 
         // when
-        service.join(member);
-        Member findMember = service.findMember(1L);
+        memberService.join(member);
+        Member findMember = memberService.findMember(1L);
 
         // then
         Assertions.assertThat(member).isEqualTo(findMember);
